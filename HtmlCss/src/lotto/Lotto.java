68d5810d946ecbed1ed2088dfd6491494eab8c00
dssh87 <dssh87@gmail.com>
2020-08-08 23:25:46 +0900

    lotto 추가

---
+++ HtmlCss/src/lotto/Lotto.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.Arrays;
+
+public class Lotto {
+
+	public static void main(String[] args) {
+		// 1~45까지 숫자배열 만들기
+		int fortyFiveNum[] = new int[45];
+		for(int i = 0; i < fortyFiveNum.length; i++) {
+			fortyFiveNum[i] = i+1;
+		}
+		System.out.println(Arrays.toString(fortyFiveNum));
+		
+		// 웹크롤링
+		
+	}
+
+}
